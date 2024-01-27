9f8aca89d3aba53d87407c5ee035d57c6df1be24
ravikumar1719 <136043341+ravikumar1719@users.noreply.github.com>
2024-01-27 22:00:15 +0530

    socket and process env update

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 app.use(cors())
 app.use(bodyParser.json())
 
-const server = app.listen(3000, () => {
+const server = app.listen(process.env.PORT || 3000, () => {
     console.log(`server is running on 3000`)
 })
 
